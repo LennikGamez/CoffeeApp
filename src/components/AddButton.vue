101e20501d40e5d0488e9030d254e986233f425f
LennikGamez <leander@doerk.org>
2024-08-17 19:18:27 +0200

    added general add button component

---
+++ src/components/AddButton.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+    const emit = defineEmits(['click']);
+</script>
+
+
+<template>
+    <button @click="emit('click')">
+        +
+    </button>
+</template>
+
+
+<style scoped>
+    button {
+        border-radius: 50px;
+        outline: none;
+        border: none;
+
+        padding: 4px 8px;
+    }
+</style>
