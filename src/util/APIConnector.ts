562479849710230de441e46f147e5a6670fd0add
LennikGamez <leander@doerk.org>
2024-08-18 14:38:22 +0200

    connector to easily get data from the api

---
+++ src/util/APIConnector.ts
@@ -0,0 +1,45 @@
+import { Bohne } from "../DB-Models";
+
+class APIConnector{
+    static SERVER = "http://localhost:3000"; 
+
+    public static endpoint(endpoint: string): string{
+        return APIConnector.SERVER + endpoint
+    }
+    public static async getBohnen(): Promise<Bohne[]>{
+        const resp = await fetch(APIConnector.endpoint("/bohnen"));
+        return await resp.json() as Bohne[];
+    }
+
+    public static async addBohnen(bohne: Bohne): Promise<boolean>{
+        const resp = await fetch(APIConnector.endpoint("/bohnen"), {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bohne)
+        })
+        return await resp.ok;
+    }
+
+    public static async updateBohnen(oldName:string, bohne: Bohne): Promise<boolean>{
+        const resp = await fetch(APIConnector.endpoint("/bohnen/" + oldName), {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bohne)
+        })
+        return await resp.ok;
+    }
+
+    public static async deleteBohnen(name: string): Promise<boolean>{
+        const resp = await fetch(APIConnector.endpoint("/bohnen/" + name), {
+            method: "DELETE"
+        })
+        return await resp.ok;
+    }
+}
+
+
+export default APIConnector
