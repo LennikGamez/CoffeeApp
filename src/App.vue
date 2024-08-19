d60e7954b751ca77bce4d972cc9c53341de0eafb
LennikGamez <leander@doerk.org>
2024-08-19 19:48:44 +0200

    basic Navbar design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+  import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <RouterView/>
+  <Navbar />
 </template>
 
 <style scoped>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+    import {  RouterLink } from 'vue-router';
+</script>
+
+
+<template>
+    <nav>
+        <RouterLink to="/" class="route">Bezug</RouterLink>
+        <RouterLink to="/add" class="route">Bohnen</RouterLink>
+    </nav>
+</template>
+
+
+<style scoped>
+
+    nav {
+        display: flex;
+        justify-content: space-around;
+        
+        position: fixed;
+        bottom: 0;
+        left: 0;
+
+        width: 100%;
+        border-top: 1px solid var(--secondary-text-color);
+    }
+
+    .route{
+        text-decoration: none;
+        color: inherit;
+        width: 100%;
+        text-align: center;
+        border-right: 1px solid var(--secondary-text-color);
+
+        transition: background-color 0.3s;
+        padding: 16px;
+
+    }
+
+    .route:hover{
+        background-color: hsla(30, 100%, 19%, 0.45);
+    }
+
+    .route:last-child{
+        border-right: none;
+    }
+    
+</style>
