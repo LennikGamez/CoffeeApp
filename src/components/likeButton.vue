ff6e639c7be509366a198e023cecf623d55eac92
LennikGamez <leander@doerk.org>
2024-08-19 22:55:08 +0200

    change of apperance

--- src/components/likeButton.vue
+++ src/components/likeButton.vue
@@ -5,12 +5,22 @@
 
 
 <template>
-    <button @click="emit('click')">
+    <button @click="emit('click')" type="button">
         🤎
     </button>
 </template>
 
 
 <style scoped>
+    button{
+        border-radius: 50px;
+        outline: none;
+        border: none;
+        padding: 12px;
+        aspect-ratio: 1;
 
+        background-color: hsl(12, 50%, 50%, 0.5);
+
+        font-size: larger;
+    }
 </style>
