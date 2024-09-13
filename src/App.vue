5ec71e30113ade57e3ce90503d596a11f75870ce
LennikGamez <leander@doerk.org>
2024-09-13 16:15:48 +0200

    first iteration of darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import Navbar from './components/Navbar.vue';
+  import ColorScheme from './components/color-scheme.vue';
 </script>
 
 <template>
     <RouterView class="content"/>
     <Navbar class="navbar"/>
+    <ColorScheme/>
 </template>
 
 <style scoped>
---
+++ src/components/color-scheme.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+
+    function toggleColorMode(){
+        document.documentElement.classList.toggle('dark');
+        localStorage.setItem('colorMode', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
+    }
+
+    onMounted(() => {
+        if(localStorage.getItem('colorMode') === 'dark'){
+            document.documentElement.classList.add('dark');
+        }
+    });
+
+</script>
+
+
+<template>
+    <div id="wrapper" @click="toggleColorMode">
+        <img src="../assets/color-schemes/lightmode.svg" id="lightmode">
+        <img src="../assets/color-schemes/darkmode.svg" id="darkmode">
+    </div>
+</template>
+
+<style scoped>
+    #wrapper{
+        user-select: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        font-size: 5rem;
+        aspect-ratio: 1;
+    }
+
+    #wrapper:hover{
+        cursor: pointer;
+    }
+
+    #lightmode, #darkmode{
+        width: 100%;
+        height: 100%;
+        display: block;
+
+        transition: transform 0.4s;
+    }
+
+    #wrapper:hover #lightmode, #wrapper:hover #darkmode{
+        transform: rotate(360deg);
+    }
+
+    html.dark #lightmode{
+        display: none;
+    }
+    html:not(.dark) #darkmode{
+        display: none;
+    }
+</style>
