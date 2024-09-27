4dbc59e6975d4f4cd3930e8eb4e3b70320b21970
Leander Doerk <leander@doerk.org>
2024-09-27 16:55:53 +0200

    keyboard komponent

---
+++ src/components/Keyboard/keyboard.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+    const keyboard = ref<HTMLDivElement | null>(null);
+
+    function hide(){
+        keyboard.value?.classList.remove("visible");
+    }
+    function show(){
+        keyboard.value?.classList.add("visible");
+    }
+
+    function appendInput(event: Event, symbol: String){
+        event.preventDefault();
+        const activeInput = document.activeElement as HTMLInputElement;
+        const cursorPosition = activeInput.selectionStart;
+        
+        // insert the symbol at cursor position
+        console.log(cursorPosition)
+        if (cursorPosition === null) return;
+        activeInput.value = activeInput.value.slice(0, cursorPosition) + symbol + activeInput.value.slice(cursorPosition);
+
+        // set cursor position
+        activeInput.selectionStart = cursorPosition + 1;
+        activeInput.selectionEnd = cursorPosition + 1;
+    }
+    function removeInput(event: Event){
+        event.preventDefault();
+        const activeInput = document.activeElement as HTMLInputElement;
+        activeInput.value = activeInput.value.slice(0, -1)
+    }
+
+    defineExpose({hide, show})
+</script>
+
+
+<template>
+    <div id="keyboard" ref="keyboard">
+        <div class="button" @mousedown='appendInput($event, "1")'>1</div>
+        <div class="button" @mousedown='appendInput($event, "2")'>2</div>
+        <div class="button" @mousedown='appendInput($event, "3")'>3</div>
+        <div class="button" @mousedown='appendInput($event, "4")'>4</div>
+        <div class="button" @mousedown='appendInput($event, "5")'>5</div>
+        <div class="button" @mousedown='appendInput($event, "6")'>6</div>
+        <div class="button" @mousedown='appendInput($event, "7")'>7</div>
+        <div class="button" @mousedown='appendInput($event, "8")'>8</div>
+        <div class="button" @mousedown='appendInput($event, "9")'>9</div>
+        <div class="button" @mousedown='appendInput($event, ".")'>.</div>
+        <div class="button" @mousedown='appendInput($event, "0")'>0</div>
+        <div @mousedown='removeInput'><--</div>
+    </div>
+</template>
+
+
+<style>
+
+    #keyboard{
+        position: absolute;
+        width: max-content;
+        height: max-content;
+        right: 50px;
+        top: 50%;
+        text-align: center;
+        display: none;
+        grid-template-columns: 1fr 1fr 1fr;
+
+        border: 1px solid black;
+        
+    }
+
+    #keyboard.visible{
+        display: grid;
+    }
+
+    #keyboard > *:hover{
+        cursor: pointer;
+        background-color: lightgray;
+        user-select: none;
+    }
+
+    #keyboard > *{
+        aspect-ratio: 1/1;
+        background-color: white;
+    }
+</style>
+
