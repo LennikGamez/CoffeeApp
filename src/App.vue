26ff26a4db04247b2d387b31848135930dfa5d93
Leander Doerk <leander@doerk.org>
2024-09-27 16:56:14 +0200

    keyboard component is shown if a input field is selected

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts">
-  import Navbar from './components/Navbar.vue';
-  import ColorScheme from './components/color-scheme.vue';
+<script lang="ts" setup>
+import Keyboard from './components/Keyboard/keyboard.vue';
+import Navbar from './components/Navbar.vue';
+import ColorScheme from './components/color-scheme.vue';
+import { ref } from 'vue';
+
+const keyboard = ref<typeof Keyboard | null>(null);
+
+  window.addEventListener("focusin", (event: Event)=>{
+    if((event.target as HTMLElement).tagName === "INPUT"){
+      // add visible clsas
+      keyboard.value?.show()
+    }
+  })
+
+  window.addEventListener("focusout", (event: Event)=>{
+    if((event.target as HTMLElement).tagName === "INPUT"){
+      // remove visible class
+      keyboard.value?.hide()
+    }
+  })
 </script>
 
 <template>
+    <Keyboard ref="keyboard"/>
     <RouterView class="content"/>
     <Navbar class="navbar"/>
     <ColorScheme/>
