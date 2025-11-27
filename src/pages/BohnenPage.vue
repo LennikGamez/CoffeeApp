361d5ce3090c9a715d354bbb4fb4544dbf8e8a40
Leander <leander@doerk.org>
2025-11-27 16:54:14 +0100

    adjusted the position of the add button

--- src/pages/BohnenPage.vue
+++ src/pages/BohnenPage.vue
@@ -1,71 +1,93 @@
 <script lang="ts" setup>
+import Bohne from "../components/BohnenComponent.vue";
+import AddButton from "../components/AddButton.vue";
+import { Bohne as BohnenType } from "../DB-Models";
+import { Ref, ref } from "vue";
+import APIConnector from "../util/APIConnector";
 
-    import Bohne from '../components/BohnenComponent.vue'
-    import AddButton from '../components/AddButton.vue';
-    import { Bohne as BohnenType } from '../DB-Models';
-    import { Ref, ref } from 'vue';
-    import APIConnector from '../util/APIConnector';
+const bohnenDataRef: Ref<BohnenType[]> = ref([] as BohnenType[]);
 
-    const bohnenDataRef: Ref<BohnenType[]> = ref([] as BohnenType[]);
-    
-    
-    function addBohnenData(data: BohnenType) {      
-        bohnenDataRef.value.push(data);
+function addBohnenData(data: BohnenType) {
+  bohnenDataRef.value.push(data);
 
-        APIConnector.addBohnen(data).then(() => fetchData());
-    }
+  APIConnector.addBohnen(data).then(() => fetchData());
+}
 
-    function fetchData() {
-        APIConnector.getBohnen().then((data) => bohnenDataRef.value = data);
-    }
+function fetchData() {
+  APIConnector.getBohnen().then((data) => (bohnenDataRef.value = data));
+}
 
-    function deleteBohne(data: BohnenType) {
-        bohnenDataRef.value = bohnenDataRef.value.filter((bohne) => data !== bohne);
-    }
+function deleteBohne(data: BohnenType) {
+  bohnenDataRef.value = bohnenDataRef.value.filter((bohne) => data !== bohne);
+}
 
-    fetchData();
-
+fetchData();
 </script>
 
-
 <template>
-    <div id="bohnen-page">
-        <h1>Bohnen</h1>
-        <div id="bohnen-list">
-            <Bohne v-for="(item, index) in bohnenDataRef" :key="index"
-                :data = "item"
-                :editMode="false"
-                @deleted="deleteBohne(item)"
-            />
-        </div>
-
-        <AddButton id="add" @click="addBohnenData({Name: 'Neue Bohne', Röster: '', Website: '', Notiz: '', VorhandendeMenge: null} as BohnenType)" />
+  <div id="bohnen-page">
+    <h1>Bohnen</h1>
+    <div id="bohnen-list">
+      <Bohne
+        v-for="(item, index) in bohnenDataRef"
+        :key="index"
+        :data="item"
+        :editMode="false"
+        @deleted="deleteBohne(item)"
+      />
     </div>
+
+    <AddButton
+      id="add"
+      @click="
+        addBohnenData({
+          Name: 'Neue Bohne',
+          Röster: '',
+          Website: '',
+          Notiz: '',
+          VorhandendeMenge: null,
+        } as BohnenType)
+      "
+    />
+  </div>
 </template>
 
-
 <style scoped>
-    #bohnen-page{
-        display: flex;
-        flex-direction: column;
-    }
-    #bohnen-list{
-        display: flex;
-        flex-wrap: wrap;
-        gap: 32px;
-        align-items: center;
-        justify-content: center;
-        
-    }
+#bohnen-page {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+}
+#bohnen-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+  align-items: center;
+  justify-content: center;
+}
 
-    #add{
-        align-self: center;
+#add {
+  position: fixed;
+  bottom: 65px;
+  right: 0;
 
-        width: fit-content;
-        padding: 16px;
-        aspect-ratio: 1;
+  align-self: center;
 
-        background-color: var(--secondary-color);
-        margin: 16px;
-    }
+  width: 42;
+  padding: 8px 16px;
+  aspect-ratio: 1/1;
+
+  background-color: var(--secondary-color);
+  margin: 16px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: transform 0.5s ease-in-out;
+}
+
+#add:hover {
+  transform: rotate(-360deg);
+}
 </style>
