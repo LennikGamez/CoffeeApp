227f3cfcd1c4328f708a8aade34d6aa99df4b9c5
Leander <leander@doerk.org>
2025-11-27 16:53:46 +0100

    Replaced text with add icon

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,22 +1,27 @@
 <script lang="ts" setup>
-
-    const emit = defineEmits(['click']);
+const emit = defineEmits(["click"]);
 </script>
 
-
 <template>
-    <button @click="emit('click')">
-        +
-    </button>
+  <button @click="emit('click')">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e3e3e3"
+    >
+      <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+    </svg>
+  </button>
 </template>
 
-
 <style scoped>
-    button {
-        border-radius: 50px;
-        outline: none;
-        border: none;
+button {
+  border-radius: 50px;
+  outline: none;
+  border: none;
 
-        padding: 4px 8px;
-    }
+  padding: 4px 8px;
+}
 </style>
